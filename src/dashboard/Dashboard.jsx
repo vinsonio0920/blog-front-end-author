6281beb90407e7f7b8af4930a3232866adc4359c
Vinson <vinsonhe2@gmail.com>
2026-04-14 14:00:18 -0700

    feat(dashboard): add dashboard JSX

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,14 +1,60 @@
 import { Link, useLoaderData } from "react-router-dom";
 import styles from "./Dashboard.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { JwtContext } from "../jwt-context";
+
+const Post = ({ post }) => {
+  return (
+    <article className={styles.postArticle}>
+      <img src={post.image} width="300" alt="Post image" />
+      <h2>{post.title}</h2>
+      <p>{post.created}</p>
+      <p>{post.description}</p>
+      <div className={styles.categoriesContainer}>
+        {post.categories.map((category) => (
+          <Link
+            to={`/categories/${category.id}`}
+            className={styles.categoryLink}
+            key={category.id}
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
+      <button type="button">
+        {post.published ? "Published" : "Not Published"}
+      </button>
+    </article>
+  );
+};
 
 const Dashboard = () => {
   const jwt = useContext(JwtContext);
+  const [page, setPage] = useState(1);
   const { result } = useLoaderData();
-  console.log(result);
 
-  const post = result.data;
+  const posts = result.data;
+  const maxPages = Math.floor(posts.length / 8) + 1;
+  let pagePosts;
+  if (posts.length > 8) {
+    pagePosts = posts.slice((page - 1) * 8, (page - 1) * 8 + 8);
+  } else {
+    pagePosts = posts;
+  }
+
+  function handlePageButtonClick(event) {
+    const buttonType = event.currentTarget.dataset.type;
+
+    if (buttonType === "previous") {
+      if (page <= 1) return;
+
+      setPage(page - 1);
+    } else {
+      if (page >= maxPages) return;
+
+      setPage(page + 1);
+    }
+  }
 
   if (!jwt.jwtToken) {
     return (
@@ -17,7 +63,7 @@ const Dashboard = () => {
         <Link to="/sign-in">Sign in now</Link>
       </div>
     );
-  } else if (post.length <= 0) {
+  } else if (posts.length <= 0) {
     return (
       <div className={styles.emptyContainer}>
         <h1 className={styles.mainHeading}>Dashboard</h1>
@@ -31,7 +77,33 @@ const Dashboard = () => {
 
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1 className={styles.mainHeading}>Dashboard</h1>
+      <ul className={styles.postUl}>
+        {pagePosts.map((post) => (
+          <li key={post.id}>
+            <Post post={post} />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.pageContainer}>
+        <button
+          className={page <= 1 ? styles.disabled : ""}
+          aria-label="Previous page"
+          data-type="previous"
+          onClick={handlePageButtonClick}
+        >
+          <span className="material-symbols-outlined">chevron_left</span>
+        </button>
+        <p>Page {page}</p>
+        <button
+          className={page >= maxPages ? styles.disabled : ""}
+          aria-label="Next page"
+          data-type="next"
+          onClick={handlePageButtonClick}
+        >
+          <span className="material-symbols-outlined">chevron_right</span>
+        </button>
+      </div>
     </>
   );
 };
