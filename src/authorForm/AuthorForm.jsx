33da12474324b3f265a95880f4e94838bc3187c8
Vinson <vinsonhe2@gmail.com>
2026-04-28 17:26:26 -0700

    feat(authorForm): add author form JSX

--- src/authorForm/AuthorForm.jsx
+++ src/authorForm/AuthorForm.jsx
@@ -1,3 +1,15 @@
-const AuthorForm = () => {};
+const AuthorForm = () => {
+  return (
+    <form>
+      <h1>Become an Author</h1>
+      <p>To become an author, enter the secret code!</p>
+      <div>
+        <label htmlFor="code">Code</label>
+        <input type="text" id="code" name="code" required maxLength="64" />
+      </div>
+      <button type="submit">Verify</button>
+    </form>
+  );
+};
 
 export { AuthorForm };
