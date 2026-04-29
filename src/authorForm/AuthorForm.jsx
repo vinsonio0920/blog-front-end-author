0dc6e13d09fa486a45883ce76288655d5b9440e6
Vinson <vinsonhe2@gmail.com>
2026-04-28 17:58:25 -0700

    style(authorForm): add author form styling

--- src/authorForm/AuthorForm.jsx
+++ src/authorForm/AuthorForm.jsx
@@ -1,13 +1,21 @@
+import styles from "./AuthorForm.module.css";
+
 const AuthorForm = () => {
   return (
-    <form>
-      <h1>Become an Author</h1>
-      <p>To become an author, enter the secret code!</p>
+    <form className={styles.authorForm}>
+      <h1 className={styles.authorHeading}>Become an Author</h1>
+      <p className={styles.authorPara}>
+        To become an author, enter the secret code!
+      </p>
       <div>
-        <label htmlFor="code">Code</label>
+        <label htmlFor="code" className="visuallyHidden">
+          Code
+        </label>
         <input type="text" id="code" name="code" required maxLength="64" />
       </div>
-      <button type="submit">Verify</button>
+      <button type="submit" className={styles.submitButton}>
+        Verify
+      </button>
     </form>
   );
 };
