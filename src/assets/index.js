255321b4074014923df727ef4d59a532f53417d5
Vinson <vinsonhe2@gmail.com>
2026-05-01 12:50:58 -0700

    chore(loader): move loader to index.html
    Since the loader doesn't actually render on the deployed site due to
    React not running yet, place loader in index.html to show loading on
    first load.

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,8 @@
 // allows images to be imported in one line instead of multiple
 import logoSvg from "./logo.svg";
-import loadingSvg from "./loading.svg";
 
 import brickWall from "./about/brick-wall.jpeg";
 
 import tripleT from "./auth/triple-t.png";
 
-export { logoSvg, loadingSvg, brickWall, tripleT };
+export { logoSvg, brickWall, tripleT };
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { routes } from "./routes.jsx";
-import { LoadingScreen } from "./loadingScreen/LoadingScreen.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} fallbackElement={<LoadingScreen />} />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
